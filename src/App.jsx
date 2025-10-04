2b73d534dc751195b569890831e4ab3c0b924d84
Justin <justin_chase_guirre@dlsl.edu.ph>
2025-10-04 15:34:47 +0800

    ???

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/Navbar/Navbar'
-import HomePage from './pages/Homepage/HomePage'
+import HomePage from './pages/Homepage/Homepage'
 import PlaceDetail from './pages/PlaceDetail/PlaceDetail'
 import BookingsPage from './pages/BookingsPage/BookingsPage'
 import PRoute from "./components/PRoute/PRoute"
